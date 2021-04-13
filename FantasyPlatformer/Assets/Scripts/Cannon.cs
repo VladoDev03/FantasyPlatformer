3efed15124eee201108339165ef4ae1c404bb272
VladoDev03 <vladsto101@gmail.com>
2021-04-13 10:02:52 +0300

    Adding cannon that is shoting towards the player.

---
+++ FantasyPlatformer/Assets/Scripts/Cannon.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float distance;
+    public float offset;
+
+    public float timeBetweenShots;
+    private float originalTimeBetweenShots;
+
+    public GameObject bullet;
+    public GameObject bulletPosition;
+    public GameObject gun;
+
+    private PlayerController player;
+
+    private bool canShoot;
+
+    private void Start()
+    {
+        canShoot = false;
+        originalTimeBetweenShots = timeBetweenShots;
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        //float rotZ = Mathf.Atan2(player.transform.rotation.y, player.transform.rotation.x) * Mathf.Rad2Deg;
+        //gun.transform.rotation = Quaternion.Euler(0, 0, rotZ + offset);
+
+        //Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector3 diff = player.transform.position - transform.position;
+        float rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        gun.transform.rotation = Quaternion.Euler(0, 0, rotZ + offset);
+
+        if (Vector2.Distance(this.transform.position, player.transform.position) <= distance)
+        {
+            canShoot = true;
+        }
+        else
+        {
+            canShoot = false;
+        }
+
+        GameObject shot = null;
+
+        if (timeBetweenShots <= 0 && canShoot)
+        {
+            shot = Instantiate(bullet, bulletPosition.transform.position, gun.transform.rotation);
+
+            timeBetweenShots = originalTimeBetweenShots;
+        }
+        else
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ FantasyPlatformer/Assets/Scripts/SpecialBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialBullet : MonoBehaviour
+{
+    public float speed;
+
+    private float timeBeforeDestroy = 3;
+
+    private PlayerController player;
+    private Vector2 target;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        target = player.transform.position;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(target, this.transform.position) == 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        this.transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (timeBeforeDestroy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            timeBeforeDestroy -= Time.deltaTime;
+        }
+    }
+}
