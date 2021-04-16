dee5c29ce7d82addf7d671218d9db5852c281448
VladoDev03 <vladsto101@gmail.com>
2021-04-16 12:39:12 +0300

    Adding prefab for phase two and three. Making the first level for the both phases.

--- FantasyPlatformer/Assets/Scripts/MenuController.cs
+++ FantasyPlatformer/Assets/Scripts/MenuController.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void Play()
+    public void PlayCave()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void PlayGround()
+    {
+        SceneManager.LoadScene("Level16");
+    }
+
+    public void PlaySky()
+    {
+        SceneManager.LoadScene("Level31");
     }
 
     public void Exit()
