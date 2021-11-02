e051417c511827515831e762f183531cb51fed9e
VladoDev03 <vladsto101@gmail.com>
2021-11-02 23:47:49 +0200

    Adding pause menu.

--- FantasyPlatformer/Assets/Scripts/GameManager.cs
+++ FantasyPlatformer/Assets/Scripts/GameManager.cs
@@ -5,16 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private bool isPaused = false;
+    public GameObject pauseMenu;
+
     private void Update()
-    {
+    {    
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Debug.Log("Exit");
-            Application.Quit();
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                isPaused = true;
+            }
         }
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //Debug.Log("Exit");
+        //Application.Quit();
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
---
+++ FantasyPlatformer/Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    
+}
